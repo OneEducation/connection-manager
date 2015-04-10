2ff003c280f6c3e9dddcdc84b395da44523de27c
Dongseok Paeng <dongseok0@gmail.com>
2015-04-10 15:20:55 +1000

    Fix multiple instance of WifiEnabler created and duplicated List fragment when back form paused status

--- connect/src/main/java/org/oneedu/connection/MainActivity.java
+++ connect/src/main/java/org/oneedu/connection/MainActivity.java
@@ -41,10 +41,6 @@ public class MainActivity extends BaseActivity {
         bindService(new Intent(this, ProxyService.class), mProxyConnection, Context.BIND_AUTO_CREATE);
         bindService(new Intent(this, WifiService.class), mWifiConnection, Context.BIND_AUTO_CREATE);
         mIsBound = true;
-
-        if (mWifiEnabler != null) {
-            mWifiEnabler.resume();
-        }
     }
 
     @Override
@@ -76,36 +72,40 @@ public class MainActivity extends BaseActivity {
         public void onServiceConnected(ComponentName className, IBinder service) {
             mWifiService = ((WifiService.LocalBinder)service).getService();
 
-            mWifiEnabler = new WifiEnabler(MainActivity.this, (CompoundButton)findViewById(R.id.sb_wifi_enable), new WifiEnabler.OnUpdateWifiStateListener() {
-                @Override
-                public void onUpdateWifiState(int state) {
-                    switch (state) {
-                        case WifiManager.WIFI_STATE_ENABLING:
-                            getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-                            mWifiStateView.setVisibility(View.VISIBLE);
-                            mWifiStateView.setText(R.string.wifi_starting);
-                            break;
-                        case WifiManager.WIFI_STATE_ENABLED:
-                            mWifiStateView.setVisibility(View.GONE);
-                            getFragmentManager().beginTransaction().addToBackStack("APList")
-                                    .replace(R.id.container, new APListFragment())
-                                    .commit();
-                            break;
-                        case WifiManager.WIFI_STATE_DISABLING:
-                            getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-                            mWifiStateView.setVisibility(View.VISIBLE);
-                            mWifiStateView.setText(R.string.wifi_stopping);
-                            break;
-                        case WifiManager.WIFI_STATE_DISABLED:
-                            getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-                            mWifiStateView.setVisibility(View.VISIBLE);
-                            mWifiStateView.setText(R.string.wifi_disabled);
-                            break;
-                        default:
-                            break;
+            if (mWifiEnabler == null) {
+                mWifiEnabler = new WifiEnabler(MainActivity.this, (CompoundButton) findViewById(R.id.sb_wifi_enable), new WifiEnabler.OnUpdateWifiStateListener() {
+                    @Override
+                    public void onUpdateWifiState(int state) {
+                        switch (state) {
+                            case WifiManager.WIFI_STATE_ENABLING:
+                                getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                                mWifiStateView.setVisibility(View.VISIBLE);
+                                mWifiStateView.setText(R.string.wifi_starting);
+                                break;
+                            case WifiManager.WIFI_STATE_ENABLED:
+                                mWifiStateView.setVisibility(View.GONE);
+                                if (getFragmentManager().getBackStackEntryCount() == 0) {
+                                    getFragmentManager().beginTransaction().addToBackStack("APList")
+                                            .replace(R.id.container, new APListFragment())
+                                            .commit();
+                                }
+                                break;
+                            case WifiManager.WIFI_STATE_DISABLING:
+                                getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                                mWifiStateView.setVisibility(View.VISIBLE);
+                                mWifiStateView.setText(R.string.wifi_stopping);
+                                break;
+                            case WifiManager.WIFI_STATE_DISABLED:
+                                getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                                mWifiStateView.setVisibility(View.VISIBLE);
+                                mWifiStateView.setText(R.string.wifi_disabled);
+                                break;
+                            default:
+                                break;
+                        }
                     }
-                }
-            });
+                });
+            }
             mWifiEnabler.resume();
         }
 
