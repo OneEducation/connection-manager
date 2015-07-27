a355555a6bdd92ac98ab8ec716aa86071872d458
Dongseok Paeng <dongseok0@gmail.com>
2015-07-27 10:25:00 +1000

    Implement auto update AppUniverse v2.0 when Connect getting update

---
+++ connect/src/main/java/org/oneedu/connection/InstallerService.java
@@ -0,0 +1,157 @@
+package org.oneedu.connection;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import org.oneedu.connection.installer.Installer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by dongseok0 on 10/07/15.
+ *
+ */
+public class InstallerService extends IntentService {
+    private final String TAG = "InstallerService";
+
+    public static final String ACTION_REINSTALL = "InstallerService.Reinstall";
+    public static final String ACTION_INSTALL = "InstallerService.Install";
+    public static final String ACTION_UNINSTALL = "InstallerService.Uninstall";
+    public static final String EXTRA_PACKAGE_NAME = "Extra.PackageName";
+    public static final String EXTRA_APK_URL = "Extra.Apk.URL";
+    public static final String EXTRA_ASSETS_APK = "Extra.Assets.Apk";
+
+    public InstallerService() {
+        super("InstallerService");
+    }
+
+    class ReInstallCallback implements Installer.InstallerCallback {
+        private Intent intent;
+
+        public ReInstallCallback(Intent intent) {
+            this.intent = intent;
+        }
+
+        @Override
+        public void onSuccess(int operation) {
+            Intent install = new Intent(getApplicationContext(), InstallerService.class);
+            install.setAction(ACTION_INSTALL);
+            install.putExtras(intent);
+            startService(install);
+        }
+
+        @Override
+        public void onError(int operation, int errorCode) {
+
+        }
+    }
+
+    @Override
+    protected void onHandleIntent(final Intent intent) {
+        Log.d(TAG, intent.toString());
+
+        switch (intent.getAction()) {
+            case ACTION_REINSTALL: {
+                try {
+                    Installer.getUnattendedInstaller(this, getPackageManager(), new ReInstallCallback(intent))
+                            .deletePackage(intent.getStringExtra(EXTRA_PACKAGE_NAME));
+                } catch (Installer.AndroidNotCompatibleException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+
+            case ACTION_INSTALL:
+                try {
+                    String filePath = null;
+                    String assetApk = intent.getStringExtra(EXTRA_ASSETS_APK);
+                    if (assetApk != null) {
+                        filePath = copyApk(assetApk);
+                    }
+
+                    if (filePath == null) {
+                        filePath = intent.getStringExtra(EXTRA_APK_URL);
+                    }
+
+                    if (filePath == null) {
+                        Log.d(TAG, "Nothing to install!");
+                        return;
+                    }
+
+                    final File apk = new File(filePath);
+                    if (!apk.exists()) {
+                        Log.d(TAG, apk + " is not exist!!");
+                        return;
+                    }
+
+                    Installer.getUnattendedInstaller(this, getPackageManager(), new Installer.InstallerCallback() {
+                        @Override
+                        public void onSuccess(int operation) {
+                            apk.delete();
+                        }
+
+                        @Override
+                        public void onError(int operation, int errorCode) {
+
+                        }
+                    }).installPackage(apk);
+                } catch (Installer.AndroidNotCompatibleException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case ACTION_UNINSTALL:
+                try {
+                    Installer.getUnattendedInstaller(this, getPackageManager(), new Installer.InstallerCallback() {
+                        @Override
+                        public void onSuccess(int operation) {
+
+                        }
+
+                        @Override
+                        public void onError(int operation, int errorCode) {
+
+                        }
+                    }).deletePackage(intent.getStringExtra(EXTRA_PACKAGE_NAME));
+                } catch (Installer.AndroidNotCompatibleException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+    @Nullable
+    private String copyApk(String uri) {
+        Log.d(TAG, "Copying asset " + uri + " to temporary");
+        AssetManager assetManager = getAssets();
+
+        try {
+            String tempPath = getExternalCacheDir() + "/temp.apk";
+            InputStream in = assetManager.open(uri);
+            OutputStream out = new FileOutputStream(tempPath);
+
+            byte[] buffer = new byte[1024];
+
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+
+            in.close();
+
+            out.flush();
+            out.close();
+            return tempPath;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- connect/src/main/java/org/oneedu/connection/PackageReceiver.java
+++ connect/src/main/java/org/oneedu/connection/PackageReceiver.java
@@ -3,24 +3,52 @@ package org.oneedu.connection;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 /**
  * Created by dongseok0 on 19/06/15.
+ *
+ * 1. Do restart proxy service.
+ * 2. Uninstall and install AppUniverse 2.0 that changed as system application.
  */
 public class PackageReceiver extends BroadcastReceiver {
+    private final String TAG = "PackageReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        Log.d("PackageReceiver", action);
+        Log.d(TAG, action);
+        Log.d(TAG, "Version : " + BuildConfig.VERSION_CODE);
 
-        if(action.equals(Intent.ACTION_PACKAGE_ADDED)){
-
-        } else if(action.equals(Intent.ACTION_PACKAGE_REMOVED)){
-
-        } else if(action.equals(Intent.ACTION_PACKAGE_REPLACED)){
-            Intent i = new Intent("org.oneedu.connection.PROXY");
-            context.startService(i);
+        switch (action) {
+            case Intent.ACTION_MY_PACKAGE_REPLACED:
+                Intent i = new Intent("org.oneedu.connection.PROXY");
+                context.startService(i);
+                checkAppUniverse(context);
+                break;
         }
+    }
+
+    private void checkAppUniverse(Context context) {
+        Intent intent = new Intent(context, InstallerService.class);
+        intent.putExtra(InstallerService.EXTRA_ASSETS_APK, "AppUniverse_v2.0.apk");
+
+        try {
+            // Skip if current version is higher than 2.0.0
+            PackageInfo info = context.getPackageManager().getPackageInfo("org.oneedu.appuniverse", 0);
+            if (info.versionCode >= 200) {
+                return;
+            }
+            // Uninstall and install if find under 2.0 version.
+            intent.setAction(InstallerService.ACTION_REINSTALL);
+            intent.putExtra(InstallerService.EXTRA_PACKAGE_NAME, "org.oneedu.appuniverse");
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d(TAG, "App Universe is not installed.");
+            intent.setAction(InstallerService.ACTION_INSTALL);
+        }
+
+        context.startService(intent);
     }
 }
